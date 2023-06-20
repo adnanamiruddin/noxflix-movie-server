8b3295b4ef9abac4e3d19a2c6499f19161701df3
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-06-20 16:16:34 +0800

    feat: Create index.js for folder routes and connect it to index.js app

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import http from "http";
 import mongoose from "mongoose";
-import "dotenv/config"
+import "dotenv/config";
+import routes from "./src/routes/index.js"
 
 const app = express()
 
