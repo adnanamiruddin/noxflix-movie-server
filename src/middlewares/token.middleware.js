9963bff4014ec256d2dfb644d983de5ac96a2406
MrAdnan34 <adnan.amiruddin34@gmail.com>
2023-06-20 16:00:01 +0800

    feat: Create route for user

--- src/middlewares/token.middleware.js
+++ src/middlewares/token.middleware.js
@@ -5,13 +5,10 @@ import userModel from "../models/user.model";
 const tokenDecode = (req) => {
   try {
     const bearerHeader = req.headers["authorization"];
-
     if (bearerHeader) {
       const token = bearerHeader.split(" ")[1];
-
       return jsonwebtoken.verify(token, process.env.SECRET_TOKEN);
     }
-
     return false;
   } catch (error) {
     return false;
@@ -20,15 +17,12 @@ const tokenDecode = (req) => {
 
 const auth = async (req, res, next) => {
   const tokenDecoded = tokenDecode(req);
-
   if (!tokenDecoded) return responseHandler.unauthorize(res);
 
   const user = await userModel.findById(tokenDecoded.data);
-
   if (!user) return responseHandler.unauthorize(res);
 
   req.user = user;
-
   next();
 };
 
---
+++ src/routes/user.route.js
@@ -0,0 +1,125 @@
+import express from "express";
+import { body } from "express-validator";
+import favoriteController from "../controllers/favorite.controller";
+import userController from "../controllers/user.controller";
+import responseHandler from "../handlers/response.handler";
+import requestHandler from "../handlers/request.handler";
+import userModel from "../models/user.model";
+import tokenMiddleware from "../middlewares/token.middleware";
+
+const router = express.Router();
+
+router.post(
+  "/signup",
+  body("username")
+    .exists()
+    .withMessage("Username is required!")
+    .isLength({ min: 8 })
+    .withMessage("Minimum 8 characters for username")
+    .custom(async (value) => {
+      const user = await userModel.findOne({ username: value });
+      if (user) return Promise.reject("Username already exist");
+    }),
+  body("password")
+    .exists()
+    .withMessage("Password is required!")
+    .isLength({ min: 8 })
+    .withMessage("Minimum 8 characters for password"),
+  body("confirmPassword")
+    .exists()
+    .withMessage("Confirm password is required!")
+    .isLength({ min: 8 })
+    .withMessage("Minimum 8 characters for password")
+    .custom((value, { req }) => {
+      if (value !== req.body.password)
+        throw new Error("Confirm password does not match with your password");
+      return true;
+    }),
+  body("displayName")
+    .exists()
+    .withMessage("Diplay name is required!")
+    .isLength({ min: 8 })
+    .withMessage("Minimum 8 characters for your display name"),
+  requestHandler.validate,
+  userController.signUp
+);
+
+router.post(
+  "/signin",
+  body("username")
+    .exists()
+    .withMessage("Username is required!")
+    .isLength({ min: 8 })
+    .withMessage("Minimum 8 characters for username"),
+  body("password")
+    .exists()
+    .withMessage("Password is required!")
+    .isLength({ min: 8 })
+    .withMessage("Minimum 8 characters for password"),
+  requestHandler.validate,
+  userController.signIn
+);
+
+router.put(
+  "/update-password",
+  tokenMiddleware.auth,
+  body("password")
+    .exists()
+    .withMessage("Your password is required!")
+    .isLength({ min: 8 })
+    .withMessage("Minimum 8 characters for password"),
+  body("newPassword")
+    .exists()
+    .withMessage("Your new password is required!")
+    .isLength({ min: 8 })
+    .withMessage("Minimum 8 characters for new password"),
+  body("confirmNewPassword")
+    .exists()
+    .withMessage("Confirm new password is required!")
+    .isLength({ min: 8 })
+    .withMessage("Minimum 8 characters for confirm new password")
+    .custom((value, { req }) => {
+      if (value !== req.body.newPassword)
+        throw new Error(
+          "Confirm new password does not match with your new password"
+        );
+      return true;
+    }),
+  requestHandler.validate,
+  userController.updatePassword
+);
+
+router.get("/info", tokenMiddleware.auth, userController.getInfo);
+
+router.get(
+  "/favorites",
+  tokenMiddleware.auth,
+  favoriteController.getUserFavorites
+);
+
+router.post(
+  "/favorites",
+  tokenMiddleware.auth,
+  body("mediaType")
+    .exists()
+    .withMessage("Media type is required!")
+    .custom((type) => ["movie", "tv"].includes(type))
+    .withMessage("Media type invalid!"),
+  body("mediaId")
+    .exists()
+    .withMessage("Media id is required!")
+    .isLength({ min: 1 })
+    .withMessage("Media id can not be empty!"),
+  body("mediaTitle").exists().withMessage("Media title is required!"),
+  body("mediaPoster").exists().withMessage("Media poster is required!"),
+  body("mediaRate").exists().withMessage("Media rate is required!"),
+  favoriteController.addFavorite
+);
+
+router.delete(
+  "/favorites/:favoriteId",
+  tokenMiddleware.auth,
+  favoriteController.removeFavorite
+);
+
+export default router;
